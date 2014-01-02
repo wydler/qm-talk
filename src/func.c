431b53434931d7ea634b8308c1fb8b5f6b709f7e
Michael Wydler <michael.wydler@googlemail.com>
2014-01-02 14:13:34 +0100

    Add example files.

---
+++ src/func.c
@@ -0,0 +1,25 @@
+#include "func.h"
+
+int analyse( char string[], char *minChar, char *maxChar )
+{
+    int i = 0;
+
+    *minChar = string[0];
+    *maxChar = string[0];
+
+    while( string[i] != '\0' )
+    {
+        if( string[i] > *maxChar )
+        {
+            *maxChar = string[i];
+        }
+        if( string[i] < *minChar )
+        {
+            *minChar = string[i];
+        }
+
+        i++;
+    }
+
+    return i;
+}
---
+++ src/func.h
@@ -0,0 +1,6 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+int analyse( char string[], char *minChar, char *maxChar );
+
+#endif /*FUNC_H*/
