7b2a2c8505e0c7ca0d5fe2cee696baf4f8915cae
Michael Wydler <michael.wydler@googlemail.com>
2014-01-02 14:58:15 +0100

    Add basic test file.

---
+++ test/funcTest.c
@@ -0,0 +1,17 @@
+#include <assert.h>
+#include "../src/func.h"
+
+int main( void )
+{
+	char string[] = "Testing";
+	char minChar = '\0';
+	char maxChar = '\0';
+	int length = 0;
+
+	length = analyse( string, &minChar, &maxChar );
+	assert( length == 7 );
+	assert( minChar == 'e' );
+	assert( maxChar == 't' );
+
+	return 0;
+}
