652e862bcbea96c30661da28c819ad1a2a8ad069
Michael Wydler <michael.wydler@googlemail.com>
2014-01-26 03:29:09 +0100

    Update demo.

--- src/coop.c
+++ src/coop.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 
-int main( void )
+int main( int argc, char *argv[] )
 {
-	printf( "Erste Nachricht\n\n" );
+	int i = 0;
 
-	printf( "Hello world\n" );
+	if( argc >= 2 ) {
+		printf( "Hello %s\n", argv[1] );
+	} else {
+		printf( "Hello World\n" );
+	}
+
+	for( i = 0; i < 10; i++ ) {
+		printf( "%d ", i);
+	}
+	printf( "\n" );
 
 	return 0;
 }
