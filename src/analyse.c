f08d4d035e8001ae594798fdd56b4f171257603f
mettenlm <mettenlm@ap055019.hs-weingarten.de>
2014-01-29 18:22:59 +0100

    Matze hats behoben!

--- src/analyse.c
+++ src/analyse.c
@@ -1,22 +1,32 @@
 #include "analyse.h"
+#include <string.h>
 
 int analyseMinMax( char string[], char * minChar, char * maxChar )
 {
 	int i = 0;
 
+    if(minChar!=NULL){
 	*minChar = string[0];
+    }
+    if(maxChar!=NULL){
 	*maxChar = string[0];
+    }
 
 	while( string[i] != '\0' )
 	{
+    if(maxChar!=NULL){
 		if( string[i] > *maxChar )
 		{
 			*maxChar = string[i];
 		}
+    }
+
+    if(minChar!=NULL){
 		if( string[i] < *minChar )
 		{
 			*minChar = string[i];
 		}
+    }
 		i++;
 	}
 
