883bd6b052c0f35af85097de1fe635e9b7a32fb1
Michael Wydler <michael.wydler@googlemail.com>
2014-01-29 09:56:34 +0100

    Change coop.

--- src/coop.c
+++ src/coop.c
@@ -3,7 +3,7 @@
 
 int main( int argc, char *argv[] )
 {
-	int i = 0;
+	int i = 1;
 
 	if( argc >= 2 ) {
 		printf( "Hello %s\n", argv[1] );
@@ -11,7 +11,7 @@ int main( int argc, char *argv[] )
 		printf( "Hello World\n" );
 	}
 
-	for( i = 0; i < 10; i++ ) {
+	for( i = 0; i <= 10; i++ ) {
 		printf( "%d ", i);
 	}
 	printf( "\n" );
